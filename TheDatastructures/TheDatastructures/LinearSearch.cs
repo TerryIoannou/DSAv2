787f7221824e35f57851653dffa34ddc23123489
liafernando <91317153+liafernando@users.noreply.github.com>
2023-06-27 21:46:52 +0000

    Data Structure
    searching algorithm and sorting algorithms implemented with the custom Data Structure.

---
+++ TheDatastructures/TheDatastructures/LinearSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDatastructures
+{
+    public static class LinearSearch
+    {
+        public static int Search(int[] arr, int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+
+}
+
---
+++ TheDatastructures/TheDatastructures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheDatastructures
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 form1 = new Form1();
+            Application.Run(form1);
+        }
+    }
+}
